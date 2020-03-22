8fd1d7fdefd1f3df28b2d51cf1f45c2461780126
DSantos44774 <A44774@alunos.isel.pt>
2020-03-22 14:50:40 +0000

    Created the project with the Bottom Navigation Template

---
+++ Project/app/src/main/java/edu/isel/ion/android/MainActivity.kt
@@ -0,0 +1,54 @@
+package edu.isel.ion.android
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupBottomBarBehaviour()
+    }
+
+    /**
+     *  Ensures that the buttons in the bottom navigation view work with the navigation controller
+     *  so that when an item is selected the fragment container is updated with the according
+     *  destination from the navigation graph. It also guarantees that the title in the action
+     *  bar is updated with the name of the selected bottom bar item.
+     *
+     *  Note : The code inside this method came with the template activity with bottom navigation
+     */
+    fun setupBottomBarBehaviour() {
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+
+
+        /**
+         * Ensures the title in the action bar is updated according to the selected item
+         * in the bottom navigation bar
+         */
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        /**
+         * Sets up an {@link OnDestinationChangedListener} on the {@link BottomNavigationView} for
+         * use with a {@link NavController}. This will call
+         * {@link #onNavDestinationSelected(MenuItem, NavController)} when a menu item is selected.
+         * The selected item in the BottomNavigationView will automatically be updated when the
+         * destination changes.
+         */
+        navView.setupWithNavController(navController)
+    }
+}
