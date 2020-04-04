6bcc85d39f69e2959ff9036b0637576fb2bb6b8a
LongJohnny <jonisilva@live.com.pt>
2020-04-04 10:08:14 +0100

    Added courses button behaviour

--- Project/app/src/main/java/edu/isel/ion/android/home/HomeFragment.kt
+++ Project/app/src/main/java/edu/isel/ion/android/home/HomeFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import edu.isel.ion.android.R
 
 class HomeFragment : Fragment() {
@@ -25,6 +27,17 @@ class HomeFragment : Fragment() {
         val viewModel = ViewModelProviders
             .of(this, HomeViewModelProvider())[HomeViewModel::class.java]
 
+        //Obtaining navigation controller, used to navigate between fragments
+        val navController = findNavController()
 
+        //Courses button
+        view.findViewById<Button>(R.id.button_home_courses).setOnClickListener {
+            navController.navigate(R.id.action_home_to_curricular_terms)
+        }
+
+        //Calendar button
+        view.findViewById<Button>(R.id.button_home_calendar).setOnClickListener {
+            TODO("Calendar fragment and navigation element")
+        }
     }
 }
