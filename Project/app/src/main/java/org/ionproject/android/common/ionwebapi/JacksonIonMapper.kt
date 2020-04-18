6205a057d0234ab55461e4786188b2a8f1586e6f
DSantos44774 <A44774@alunos.isel.pt>
2020-04-18 11:59:35 +0100

    Removed debug code

--- Project/app/src/main/java/org/ionproject/android/common/ionwebapi/JacksonIonMapper.kt
+++ Project/app/src/main/java/org/ionproject/android/common/ionwebapi/JacksonIonMapper.kt
@@ -1,6 +1,5 @@
 package org.ionproject.android.common.ionwebapi
 
-import android.util.Log
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import kotlinx.coroutines.Dispatchers
@@ -19,19 +18,7 @@ class JacksonIonMapper : IIonMapper {
      */
     override suspend fun parse(responseBody: String): SirenEntity =
         withContext(Dispatchers.Default) {
-            Log.println(Log.DEBUG, "COURSES", "Started parsing to SirenEntity")
-            val res = kotlin.runCatching {
-                jacksonObjMapper.readValue<SirenEntity>(responseBody)
-            }
-            if (res.isFailure) {
-                Log.println(
-                    Log.DEBUG,
-                    "COURSES",
-                    "Failed parsing to SirenEntity, exception : ${res.exceptionOrNull()}"
-                )
-                throw res.exceptionOrNull()!! //This will never return null here
-            }
-            Log.println(Log.DEBUG, "COURSES", "Successfully parsed to SirenEntity")
-            res.getOrNull()!!
+            jacksonObjMapper.readValue<SirenEntity>(responseBody)
+
         }
 }
