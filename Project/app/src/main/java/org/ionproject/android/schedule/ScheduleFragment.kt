f71b5cceaa96fa783596fe9d2c14a19610c88cc1
LongJohnny <jonisilva@live.com.pt>
2020-04-24 15:57:13 +0100

    Created Moment class and improved layout for schedule

--- Project/app/src/main/java/org/ionproject/android/schedule/ScheduleFragment.kt
+++ Project/app/src/main/java/org/ionproject/android/schedule/ScheduleFragment.kt
@@ -9,7 +9,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_schedule.*
 import kotlinx.android.synthetic.main.fragment_schedule.view.*
 import org.ionproject.android.MainActivity
 import org.ionproject.android.R
@@ -33,14 +35,22 @@ class ScheduleFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         activity?.bottomnavview_main?.visibility = View.GONE
-        (activity as MainActivity)?.supportActionBar?.hide()
-        activity?.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
+        (activity as MainActivity).supportActionBar?.hide()
+        activity?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
         view.button_schedule_back.setOnClickListener {
             activity?.bottomnavview_main?.visibility = View.VISIBLE
-            (activity as MainActivity)?.supportActionBar?.show()
-            activity?.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+            (activity as MainActivity).supportActionBar?.show()
+            activity?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
             findNavController().navigateUp()
         }
+
+        val intervalsList = recyclerview_schedule_hours
+        intervalsList.layoutManager = LinearLayoutManager(context)
+        intervalsList.adapter = HoursListAdapter(
+            startHours = Moment(8, 0),
+            endHours = Moment(23, 0)
+        )
+
     }
 
 }
---
+++ Project/app/src/test/java/org/ionproject/android/MomentUnitTests.kt
@@ -0,0 +1,38 @@
+package org.ionproject.android
+
+import org.ionproject.android.schedule.Moment
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MomentUnitTests {
+
+    @Test
+    fun addition_isCorrect() {
+        val moment = Moment.ThirtyMinutes +Moment.ThirtyMinutes + Moment.OneHour
+        assertEquals(2, moment.hours)
+    }
+
+    @Test
+    fun additionOverflow_isCorrect() {
+        val moment = Moment(23,0)
+        val newMoment = moment + Moment.OneHour
+        assertEquals(1, newMoment.hours)
+    }
+
+    @Test
+    fun until_isCorrect() {
+        val start = Moment(1,0)
+        val end = Moment(2,0)
+        val momentsList = start until end
+        assertEquals(61, momentsList.count())
+    }
+
+    @Test
+    fun untilWithStep_isCorrect() {
+        val start = Moment(8,0)
+        val end = Moment(23,0)
+        val momentsList = start until end step Moment.ThirtyMinutes
+        assertEquals(31, momentsList.count())
+    }
+}
