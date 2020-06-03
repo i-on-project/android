97018df7e8d2e7b0faea33a6295068804202a9cd
LongJohnny <jonisilva@live.com.pt>
2020-06-03 18:41:23 +0100

    Finished hole app navigation test

--- Project/app/src/androidTest/java/org/ionproject/android/NavigationTests.kt
+++ Project/app/src/androidTest/java/org/ionproject/android/NavigationTests.kt
@@ -1,13 +1,24 @@
 package org.ionproject.android
 
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.ViewInteraction
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
+import org.hamcrest.CoreMatchers.allOf
+import org.hamcrest.Matcher
+import org.ionproject.android.course_details.ClassesListAdapter
+import org.ionproject.android.courses.CoursesListAdapter
+import org.ionproject.android.programmeDetails.TermsListAdapter
+import org.ionproject.android.programmes.ProgrammesListAdapter
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -52,51 +63,91 @@ class NavigationTests {
 
     @Test
     fun canNavigateToCourseDetails() {
+        // Navigate to programmes fragment
         onView(withId(R.id.button_home_programmes)).perform(click())
-        val hasData = CompletableFuture<Boolean>()
-        onView(withId(R.id.recyclerview_programmes_list)).check { view, noViewFoundException ->
-            val adapter = (view as RecyclerView).adapter
-            if (adapter != null) {
-                if (adapter.itemCount > 0) {
-                    hasData.complete(true)
-                } else {
-                    (view as RecyclerView).adapter?.registerAdapterDataObserver(
-                        AdapterObserver {
-                            hasData.complete(true)
-                        })
-                }
-            }
+        onView(withId(R.id.fragment_programmes)).check(matches(isDisplayed()))
 
-        }
-        hasData.join()
-        onView(withText("LEIC")).check(matches(isDisplayed()))
-        onView(withText("MEIC")).check(matches(isDisplayed()))
+        // Navigate to programme details
+        onView(withId(R.id.recyclerview_programmes_list)).waitForData().perform(
+            RecyclerViewActions.actionOnItemAtPosition<ProgrammesListAdapter.ProgrammeViewHolder>(
+                0,
+                click()
+            )
+        )
+        onView(withId(R.id.fragment_programme_details)).check(matches(isDisplayed()))
+
+        // Navigate to courses
+        onView(withId(R.id.recyclerview_programme_details_terms)).waitForData().perform(
+            RecyclerViewActions.actionOnItemAtPosition<TermsListAdapter.TermViewHolder>(
+                0,
+                click()
+            )
+        )
+        onView(withId(R.id.fragment_courses)).check(matches(isDisplayed()))
+
+        // Navigate to course details
+        onView(withId(R.id.recyclerview_courses_list)).waitForData().perform(
+            RecyclerViewActions.actionOnItemAtPosition<CoursesListAdapter.CourseViewHolder>(
+                0,
+                click()
+            )
+        )
+        onView(withId(R.id.fragment_course_details)).check(matches(isDisplayed()))
+
+        // Navigate to class section
+        onView(withId(R.id.recyclerview_course_details_classes_list)).waitForData().perform(
+            RecyclerViewActions.actionOnItemAtPosition<ClassesListAdapter.ClassViewHolder>(
+                0,
+                click()
+            )
+        )
+        onView(withId(R.id.fragment_class_section)).check(matches(isDisplayed()))
     }
-
-    @Test
-    fun canNavigateToCourses() {
-        onView(withId(R.id.button_home_programmes)).perform(click())
-        Thread.sleep(5000)
-        onView(withText("LEIC")).perform(click())
-        Thread.sleep(5000)
-        onView(withText("1 TERM")).perform(click())
-        Thread.sleep(5000)
-        onView(withText("PG")).check(matches(isDisplayed()))
-    }
-
-    /*@Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("org.ionproject.android", appContext.packageName)
-    }*/
 }
 
-class AdapterObserver(val onChanged2: () -> Unit) : RecyclerView.AdapterDataObserver() {
+class AdapterObserver(val onUpdate: () -> Unit) : RecyclerView.AdapterDataObserver() {
 
     override fun onChanged() {
         super.onChanged()
-        onChanged2()
+        this.onUpdate()
+    }
+
+}
+
+fun ViewInteraction.waitForData(): ViewInteraction {
+    val completableFuture = CompletableFuture<Boolean>()
+    this.perform(waitForData {
+        completableFuture.complete(true)
+    })
+    completableFuture.join()
+    return this
+}
+
+
+class waitForData(private val onUpdate: () -> Unit) : ViewAction {
+
+    override fun getDescription(): String? {
+        return null
+    }
+
+    override fun getConstraints(): Matcher<View> =
+        allOf(isAssignableFrom(RecyclerView::class.java))
+
+
+    override fun perform(uiController: UiController, view: View) {
+
+        val adapter = (view as RecyclerView).adapter
+        if (adapter != null) {
+            if (adapter.itemCount > 0) {
+                onUpdate()
+            } else {
+                adapter.registerAdapterDataObserver(
+                    AdapterObserver {
+                        onUpdate()
+                    })
+            }
+        }
+
     }
 
 }
