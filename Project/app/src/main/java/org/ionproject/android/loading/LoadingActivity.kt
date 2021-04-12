c5ede73041ac73c4b544702bbcd938bfac8e3c35
Jtoliveira <joaotiagomingachos@hotmail.com>
2021-04-12 14:17:46 +0100

    Add connectivity check to draft contribution

--- Project/app/src/main/java/org/ionproject/android/loading/LoadingActivity.kt
+++ Project/app/src/main/java/org/ionproject/android/loading/LoadingActivity.kt
@@ -3,11 +3,11 @@ package org.ionproject.android.loading
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.activity_loading.*
 import org.ionproject.android.ExceptionHandlingActivity
 import org.ionproject.android.R
+import org.ionproject.android.common.IonApplication
 import org.ionproject.android.common.addGradientBackground
 import org.ionproject.android.common.model.Root
 import org.ionproject.android.error.ERROR_KEY
@@ -18,6 +18,12 @@ import java.net.URI
 // Random value key used to pass the root object from [LoadingActivity] to [MainActivity] via the intent
 const val ROOT_KEY = "m0192exe1gxe12x1"
 
+/**
+ * 1) Try and get data from the JsonHome URL
+ * 2) if it fails, get the URL from the Remote Config file
+ * 3) it it fails to get that info, check connectivity: open isel's page if it exists, redirect to no connect
+ * error if it doesn't
+ */
 class LoadingActivity : ExceptionHandlingActivity() {
 
     private val loadingViewModel by lazy(LazyThreadSafetyMode.NONE) {
@@ -29,7 +35,15 @@ class LoadingActivity : ExceptionHandlingActivity() {
         setContentView(R.layout.activity_loading)
         linearlayout_activity_loading.addGradientBackground()
 
-        // If JsonHome contains all required resources then open main activity else app must be outdated
+        /**
+         * If JsonHome contains all required resources then open main activity else app must be outdated
+         *
+         * We check if remote config live data is null bc this observer is used for all requests made to
+         * the API: if it is the first request ever made, get the remote config
+         * (the live data is null at this point); if it is the request after getting remote config,
+         * and it fails again, redirect to ISEL's page
+         *
+         */
         loadingViewModel.observeRootLiveData(this) {
             when (it) {
                 is FetchSuccess<Root> -> {
@@ -38,32 +52,34 @@ class LoadingActivity : ExceptionHandlingActivity() {
                     this.startActivity(intent)
                 }
                 is FetchFailure<Root> -> {
-                    if (loadingViewModel.getRemoteConfigLiveData() == null)
+                    if (loadingViewModel.getRemoteConfigLiveData() == null) {
                         loadingViewModel.getRemoteConfig()
-                    else
-                        openingTheBrowser()
+                    }else {
+                        openISELPage()
+                    }
                 }
             }
         }
 
-        //by this point remoteConfig already has the valid link
+        /**
+         * We check the connectivity in this observer because, although unlikely, GitHub might
+         * be down and there needs to be a plan for that
+         */
         loadingViewModel.observeRemoteConfigLiveData(this){
-            Log.d("API","Remote Config returned")
             when (it) {
                 is FetchSuccess<RemoteConfig> -> loadingViewModel.getJsonHome(URI(it.value.api_link))
                 is FetchFailure<RemoteConfig> -> {
-                    // TODO: Check if we have connectivity
-                    // If not, start error activity with a no connectivity message
-                    startActivity(
-                        Intent(this, ErrorActivity::class.java)
-                            .putExtra(
-                                ERROR_KEY,
-                                resources.getString(R.string.label_no_connectivity_loading_error)
-                            )
-                    )
-                    // If yes, open in browser
-                    // TODO:
-                    openingTheBrowser()
+                    if(!IonApplication.connectivityObservable.hasConnectivity() ){
+                        startActivity(
+                            Intent(this, ErrorActivity::class.java)
+                                .putExtra(
+                                    ERROR_KEY,
+                                    resources.getString(R.string.label_no_connectivity_loading_error)
+                                )
+                        )
+                    }else{
+                        openISELPage()
+                    }
                 }
             }
         }
@@ -71,8 +87,9 @@ class LoadingActivity : ExceptionHandlingActivity() {
 
     //if the remote config link is the same as the link in the app,
     //it means the API is down and for now we redirect to the ISEL website
-    private fun openingTheBrowser(){
+    private fun openISELPage(){
         val iselURL = "https://www.isel.pt"
         startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(iselURL)))
     }
+
 }
--- Project/app/src/main/java/org/ionproject/android/loading/LoadingViewModel.kt
+++ Project/app/src/main/java/org/ionproject/android/loading/LoadingViewModel.kt
@@ -9,6 +9,17 @@ import java.net.URI
 // This uri has to be hardcoded there is no other way
 private val ROOT_URI_V0 = URI("/")
 
+/*
+getJsonHome() returns null when the API is unavailable. This makes the default null status of
+rootLiveData ambiguous, so thsi class envelops the results of the getJsonHome() method in order to
+properly evaluate the state of the API and to launch the Remote Config strat
+
+Fetch Failure with null value: request was made to API and there was no response
+Fetch Failure with throwable: request threw an Exception, if not caught triggers the global Exception Handler
+from ExceptionHandlingActivity()
+
+Fetch Success : valid response
+ */
 sealed class FetchResult<out T>
 data class FetchFailure<T>(val throwable: Throwable? = null) : FetchResult<T>()
 data class FetchSuccess<T>(val value: T) : FetchResult<T>()
@@ -25,9 +36,6 @@ class LoadingViewModel(
     }
 
     /*
-    Added a try catch bc the automated Exception handling done by
-    ExceptionHandlingActivity.kt wasn't allowing for the if in the observer to function
-
     Retrofit note: since retrofit 2 uses okhttp's HttpUrl, we don't need to change the base url of
     the service in endpoints where the method annotation doesn't specify a url. it detects if the
     @URL parameter doesn't match with the base url and sorts everything out by itself
@@ -49,7 +57,7 @@ class LoadingViewModel(
         viewModelScope.launch {
             val result = try {
                 val remoteConfig = remoteConfigRepository.getRemoteConfig()
-                if (remoteConfig != null) FetchSuccess(remoteConfig) else FetchFailure<RemoteConfig>()
+                if (remoteConfig?.api_link != null) FetchSuccess(remoteConfig) else FetchFailure<RemoteConfig>()
             } catch(e: Exception) {
                 FetchFailure<RemoteConfig>(e)
             }
